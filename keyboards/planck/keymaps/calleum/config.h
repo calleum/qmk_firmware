304f5adb288a1e6a8c8352477ee51e2e16c5099d
Calleum Pecqueux <hi@calleum.au>
2024-07-14 11:53:59 +1000

    feat(keymap): Add personal keymap for plank_v6drop
    * update with sounds and updated map for bottom row!

--- keyboards/planck/keymaps/calleum/config.h
+++ keyboards/planck/keymaps/calleum/config.h
@@ -19,7 +19,7 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(CAMPANELLA)
+#    define STARTUP_SONG SONG(CLUEBOARD_SOUND)
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
